f3db334e0080caadebb18d68216e2bc7f2a60c0b
IvanSusanto05 <119504327+IvanSusanto05@users.noreply.github.com>
2023-03-21 20:27:09 +0700

    CV

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,169 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Ivan Susanto | CV</title>
+    <link
+      rel="stylesheet"
+      href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css"
+      integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm"
+      crossorigin="anonymous"
+    />
+    <style>
+      #padding div {
+        margin-left: -15px;
+      }
+    </style>
+  </head>
+  <body>
+    <div class="container mt-3">
+      <div class="row">
+        <div class="col-md-4 col-sm-12 mb-3">
+          <div class="container border">
+            <div class="row">
+              <div class="col-12 mt-5">
+                <img src="img/ipang.jpg" alt="" class="w-100" />
+              </div>
+              <div class="col-12 my-1">
+                <h2>Ivan Susanto</h2>
+              </div>
+              <div class="col-12 my-1">
+                <p>19 Tahun</p>
+              </div>
+              <div class="col-12 my-1 border-bottom">
+                <p>Designer</p>
+              </div>
+              <div class="col-12 my-1">
+                <h2>Skill</h2>
+                <p>Adobe XD</p>
+                <div class="progress">
+                  <div
+                    class="progress-bar"
+                    role="progressbar"
+                    style="width: 60%"
+                    aria-valuenow="75"
+                    aria-valuemin="0"
+                    aria-valuemax="100"
+                  >
+                    60%
+                  </div>
+                </div>
+                <p class="pt-4">Adobe Illustrator</p>
+                <div class="progress mb-1">
+                  <div
+                    class="progress-bar"
+                    role="progressbar"
+                    style="width: 70%"
+                    aria-valuenow="100"
+                    aria-valuemin="0"
+                    aria-valuemax="100"
+                  >
+                    70%
+                  </div>
+                </div>
+                <p class="pt-4">Adobe Photoshop </p>
+                <div class="progress mb-1">
+                  <div
+                    class="progress-bar"
+                    role="progressbar"
+                    style="width: 70%"
+                    aria-valuenow="100"
+                    aria-valuemin="0"
+                    aria-valuemax="100"
+                  >
+                    70%
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-8 col-sm-12">
+          <div class="container border">
+            <div class="row">
+              <div class="col">
+                <div class="container" id="padding">
+                  <div class="row">
+                    <div class="col-12 mt-5">
+                      <h2>Pencapaian</h2>
+                    </div>
+                    <div class="col-12 my-2">
+                      <h5>Juara 3 Ananda Cup bagian Cinematography</h5>
+                    </div>
+                    <div class="col-12">
+                      <a href ="https://www.youtube.com/watch?v=slAu9GgFyCY&ab_channel=%7CSMAKYS%7CSMAKYOSSUDARSOBATAM" class="btn btn-outline-secondary">Watch Full</a>
+                  </div>
+                </div>
+                <div class="container" id="padding">
+                  <div class="row">
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="container border my-3">
+            <div class="row">
+              <div class="col">
+                <div class="container">
+                  <div class="row" id="padding">
+                    <div class="col-12 mt-5">
+                      <h2>Edukasi</h2>
+                    </div>
+                    <div class="col-12 my-2">
+                      <h5>SMP Ananda Batam</h5>
+                    </div>
+                    <div class="col-12">
+                      <h6>2016-2019</h6>
+                    </div>
+                    <div class="col-12 border-bottom">
+                      <p>
+                        Belajar tentang pahitnya kehidupan
+                      </p>
+                    </div>
+                  </div>
+                </div>
+                <div class="container">
+                  <div class="row" id="padding">
+                    <div class="col-12 my-2">
+                      <h5>SMK Maitreyawira Batam</h5>
+                    </div>
+                    <div class="col-12">
+                      <h6>2019-2022</h6>
+                    </div>
+                    <div class="col-12 border-bottom">
+                      <p>
+                        Belajar tentang tidak boleh menaruk ekspetasi tinggi ke seseorang
+                      </p>
+                    </div>
+                  </div>
+                </div>
+                <div class="container">
+                  <div class="row" id="padding">
+                    <div class="col-12 my-2">
+                      <h5>UIB</h5>
+                    </div>
+                    <div class="col-12">
+                      <h6>2022-Sekarang</h6>
+                    </div>
+                    <div class="col-12 mb-3">
+                      <p>
+                        Belajar menjadi Extrovert
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <script
+      src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js"
+      integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl"
+      crossorigin="anonymous"
+    ></script>
+  </body>
+</html>
